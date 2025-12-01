5fb4b797dcd0dfb9cd7caf225deedd27a654bf24
Rainan Jorge <rainan14ip@gmail.com>
2025-11-30 21:10:51 -0300

    Adicionando arquivos

---
+++ chargeManager/src/main/java/dto/ChargeDTO.java
@@ -0,0 +1,12 @@
+package dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class ChargeDTO {
+    private Long id;
+    private BigDecimal amount;
+    private String customerName;
+    private String status;
+}
